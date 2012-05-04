6c8854f66f6d149e6292a278b40ba3b9b2c386f6
bujlder <bujlder@gmail.com>
2012-05-04 18:32:50 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bujlog::Application.config.secret_token = 'f924c0cc0d14fd58ce67102b0f9687eda48f4cc9846a235d049c0b9d6e030149a199d40df1fa4e9ae437274218a72bc2b3cacfc270c75829f2b38f2423c2fc0d'
